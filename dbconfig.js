f687d4f943b5e25dc3cdc43bafc75a26fe19c057
vinayakjain <vinaaykjain388@gmail.com>
2025-01-27 03:09:01 +0530

    update stock feat fix again

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 async function callDb(){
     try{
-        const mongoUri='mongodb://localhost:27017/Shop'
+        const mongoUri='mongodb+srv://shopDB:shopDB@vinayak.g0lxnfc.mongodb.net/shopDB'
        const isConnected=await mongoose.connect(mongoUri);
        if(!isConnected){
         throw new Error("failde")
