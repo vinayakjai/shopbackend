39a7fa5d9f23c0a8aed1ef8e072d75382dc35fb1
vinayakjain <vinaaykjain388@gmail.com>
2025-06-22 11:48:40 +0530

    solved cors issue

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app=express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
-    origin:"https://myhomeshopxyz123.netlify.app"
+    origin:"https://myhomeshopxyz123.netlify.app/"
 
 }))
 
